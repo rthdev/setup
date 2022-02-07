a80e9ecc3e20d3e7211332950646c9d69f23330a
rthdev <37539286+rthdev@users.noreply.github.com>
2022-02-07 16:09:27 +0100

    fix file endings

--- workstation.sh
+++ workstation.sh
@@ -4,4 +4,4 @@ sudo yum -y install vim git ansible
 
 git clone https://github.com/rthdev/rthap_workstation.git
 
-cd rthap_workstation && ansible-playbook site.yaml
+cd rthap_workstation && ansible-playbook site.yml
