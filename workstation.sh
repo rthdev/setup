7ce09000b8511e7409e1243dba767a0d5152a2fb
rthdev <37539286+rthdev@users.noreply.github.com>
2022-02-07 16:11:41 +0100

    add a repos subdir

--- workstation.sh
+++ workstation.sh
@@ -2,6 +2,6 @@
 
 sudo yum -y install vim git ansible
 
-git clone https://github.com/rthdev/rthap_workstation.git
-
+mkdir ${HOME}/repos
+cd ${HOME}/repos && git clone https://github.com/rthdev/rthap_workstation.git
 cd rthap_workstation && ansible-playbook site.yml
