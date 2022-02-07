0e3f98f07dc9e68af35430037528ef2ab4f03e80
rthdev <37539286+rthdev@users.noreply.github.com>
2022-02-07 16:19:07 +0100

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -2,6 +2,14 @@
 
 sudo yum -y install vim git ansible
 
-mkdir ${HOME}/repos
-cd ${HOME}/repos && git clone https://github.com/rthdev/rthap_workstation.git
+if [ -z "${HOME}" ]
+then
+  echo "ERROR: HOME variable not defined, exiting..."
+  exit 1
+fi
+  
+mkdir ${HOME}/repos && \
+cd ${HOME}/repos && \
+git clone https://github.com/rthdev/rthap_workstation.git
+
 cd rthap_workstation && ansible-playbook site.yml
